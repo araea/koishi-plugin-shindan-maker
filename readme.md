bd06d062d1859f6c4839699a56c0a76413e8cfb0
Nawyjx <3373167460@qq.com>
2024-02-06 15:06:32 +0800

    Update

--- readme.md
+++ readme.md
@@ -4,7 +4,10 @@
 
 ## 🎈 介绍
 
-koishi-plugin-shindan-maker 是一个基于 [Koishi](https://koishi.chat/) 的插件，可以让你的机器人使用 [神断网](https://en.shindanmaker.com/) 上的各种有趣的测试和生成器，包括文字和图片模式。你可以随机抽取一个神断，也可以自定义添加或删除神断，还可以修改神断的指令和模式。无论你想要生成一个 OC 角色，还是想要抽取一个老婆，或者想要卖个萌，这个插件都能满足你的需求。😉
+koishi-plugin-shindan-maker 是一个基于 [Koishi](https://koishi.chat/)
+的插件，可以让你的机器人使用 [神断网](https://en.shindanmaker.com/)
+上的各种有趣的测试和生成器，包括文字和图片模式。你可以随机抽取一个神断，也可以自定义添加或删除神断，还可以修改神断的指令和模式。无论你想要生成一个
+OC 角色，还是想要抽取一个老婆，或者想要卖个萌，这个插件都能满足你的需求。😉
 
 ## 📦 安装
 
@@ -27,28 +30,62 @@ npm install koishi-plugin-shindan-maker
 
 ## ⚙️ 配置项
 
-- `isOfficialShindanSyncEnabled`：是否与插件内置神断保持同步，关闭后，将不会再为你新增任何神断，默认为 `true`。
-- `shindanUrl`：神断网的 URL，可选前缀有 `en`，`kr`，`cn`，`th`，或者无前缀（默认为 `en`）。
-- `maxRetryCount`：最大重试次数，当请求神断网失败时，会尝试重新请求，直到达到最大重试次数（默认为 `3`）。
-- `imageType`：图片模式生成的图片类型，可选值有 `png`，`jpeg`，`webp`（默认为 `png`）。
-- `isRandomDivineCommandVisible`：随机神断的时候是否显示神断指令名，默认为 `true`。
-- `defaultMaxDisplayCount`：排行榜默认显示的人数，默认为 `10`。
-- `defaultShindansBatchCount`：发送神断列表默认的批次数，最大值为 `10`，默认为 `5`。
-- `shouldMiddlewareInterruptAfterDivineDirective`：中间件是否在获取神断指令之后中断，默认为 `true`。
+### 神断同步设置
 
-## 📝 命令
+- `isOfficialShindanSyncEnabled`：是否与插件内置神断保持同步。关闭后，将不再为你新增任何神断。默认为 `true`。
+
+### 神断网 URL
+
+- `shindanUrl`：神断网的 URL。可选前缀有 `en`，`kr`，`cn`，`th`，或者无前缀（默认为 `en`）。
+
+### 请求设置
+
+- `maxRetryCount`：最大重试次数。当请求神断网失败时，会尝试重新请求，直到达到最大重试次数（默认为 `3`）。
+
+### 图片模式设置
+
+- `imageType`：图片模式生成的图片类型。可选值有 `png`，`jpeg`，`webp`（默认为 `png`）。
+
+### 随机神断设置
+
+- `isRandomDivineCommandVisible`：随机神断的时候是否显示神断指令名。默认为 `true`。
+
+### 排行榜设置
+
+- `defaultMaxDisplayCount`：排行榜默认显示的人数。默认为 `10`。
+
+### 神断列表设置
+
+- `defaultShindansBatchCount`：发送神断列表默认的批次数。最大值为 `10`，默认为 `5`。
+
+### 中间件设置
+
+- `shouldMiddlewareInterruptAfterDivineDirective`：中间件是否在获取神断指令之后中断。默认为 `true`。
+
+## 📝 神断命令
+
+### 查看信息
 
 - `shindan`：查看神断帮助信息。
-- `shindan.列表 [batchCount:number]`：查看神断列表，可以指定批次数，每批显示的神断数量（默认为 `5`）。
+- `shindan.列表 [batchCount:number]`：查看神断列表，可指定批次数和每批显示的神断数量（默认为 `5`）。
+
+### 神断操作
+
 - `shindan.查看 [shindanCommand:text]`：根据神断指令查看神断信息。
 - `shindan.排行榜 [number:number]`：查看神断次数排行榜，number 参数为排行榜显示人数，默认为 `20`。
 - `shindan.统计 [targetUser:text]`：查看指定用户的神断统计信息。
-- `shindan.随机 [shindanName:text]`：随机抽取一个神断，可以指定神断名，如果不指定则使用用户名（默认为 `text` 模式）。
-- `shindan.添加 <shindanId:string> <shindanCommand:string> [shindanMode:string]`：添加一个神断到列表中，需要指定神断 ID，神断指令，和神断模式（默认为 `image` 模式）。
-- `shindan.删除 <shindanCommand:string>`：删除列表中的一个神断，需要指定神断指令。
-- `shindan.修改 <shindanCommand:string> <shindanNewCommand:string> [shindanMode:string]`：修改列表中的一个神断，需要指定原神断指令，新神断指令，和神断模式（默认为原神断模式）。
-- `shindan.设置 <shindanCommand:string> <shindanMode:string>`：设置列表中的一个神断的模式，需要指定神断指令和神断模式。
-- `shindan.自定义 <shindanId:string> [shindanName:string] [shindanMode:string]`：自定义生成一个神断结果，需要指定神断 ID，可以指定神断名，如果不指定则使用用户名，可以指定神断模式（默认为 `image` 模式）。
+- `shindan.随机 [shindanName:text]`：随机抽取一个神断，可指定神断名，若不指定则使用用户名（默认为 `text` 模式）。
+- `shindan.自定义 <shindanId:string> [shindanName:string] [shindanMode:string]`：自定义生成一个神断结果，需指定神断
+  ID，可指定神断名，若不指定则使用用户名，可指定神断模式（默认为 `image` 模式）。
+
+### 神断管理
+
+- `shindan.添加 <shindanId:string> <shindanCommand:string> [shindanMode:string]`：添加一个神断到列表中，需指定神断
+  ID、神断指令和神断模式（默认为 `image` 模式）。
+- `shindan.删除 <shindanCommand:string>`：删除列表中的一个神断，需指定神断指令。
+- `shindan.修改 <shindanCommand:string> <shindanNewCommand:string> [shindanMode:string]`
+  ：修改列表中的一个神断，需指定原神断指令、新神断指令和神断模式（默认为原神断模式）。
+- `shindan.设置 <shindanCommand:string> <shindanMode:string>`：设置列表中的一个神断的模式，需指定神断指令和神断模式。
 
 ## 🙏 致谢
 
--- src/index.ts
+++ src/index.ts
@@ -21,19 +21,24 @@ export const usage = `## 🎮 使用
 - 你可以使用 \`-t\` 或 \`-i\` 参数，指定生成的神断结果是文本模式还是图片模式。
 - 你可以使用 \`shindan.添加\`，\`shindan.删除\`，\`shindan.修改\` 和 \`shindan.设置\` 指令，管理你的神断列表。
 
-## 📝 命令
+## 📝 神断命令
 
+### 查看信息
 - \`shindan\`：查看神断帮助信息。
-- \`shindan.列表 [batchCount:number]\`：查看神断列表，可以指定批次数，每批显示的神断数量（默认为 \`5\`）。
+- \`shindan.列表 [batchCount:number]\`：查看神断列表，可指定批次数和每批显示的神断数量（默认为 \`5\`）。
+
+### 神断操作
 - \`shindan.查看 [shindanCommand:text]\`：根据神断指令查看神断信息。
 - \`shindan.排行榜 [number:number]\`：查看神断次数排行榜，number 参数为排行榜显示人数，默认为 \`20\`。
 - \`shindan.统计 [targetUser:text]\`：查看指定用户的神断统计信息。
-- \`shindan.随机 [shindanName:text]\`：随机抽取一个神断，可以指定神断名，如果不指定则使用用户名（默认为 \`text\` 模式）。
-- \`shindan.添加 <shindanId:string> <shindanCommand:string> [shindanMode:string]\`：添加一个神断到列表中，需要指定神断 ID，神断指令，和神断模式（默认为 \`image\` 模式）。
-- \`shindan.删除 <shindanCommand:string>\`：删除列表中的一个神断，需要指定神断指令。
-- \`shindan.修改 <shindanCommand:string> <shindanNewCommand:string> [shindanMode:string]\`：修改列表中的一个神断，需要指定原神断指令，新神断指令，和神断模式（默认为原神断模式）。
-- \`shindan.设置 <shindanCommand:string> <shindanMode:string>\`：设置列表中的一个神断的模式，需要指定神断指令和神断模式。
-- \`shindan.自定义 <shindanId:string> [shindanName:string] [shindanMode:string]\`：自定义生成一个神断结果，需要指定神断 ID，可以指定神断名，如果不指定则使用用户名，可以指定神断模式（默认为 \`image\` 模式）。
+- \`shindan.随机 [shindanName:text]\`：随机抽取一个神断，可指定神断名，若不指定则使用用户名（默认为 \`text\` 模式）。
+- \`shindan.自定义 <shindanId:string> [shindanName:string] [shindanMode:string]\`：自定义生成一个神断结果，需指定神断 ID，可指定神断名，若不指定则使用用户名，可指定神断模式（默认为 \`image\` 模式）。
+
+### 神断管理
+- \`shindan.添加 <shindanId:string> <shindanCommand:string> [shindanMode:string]\`：添加一个神断到列表中，需指定神断 ID、神断指令和神断模式（默认为 \`image\` 模式）。
+- \`shindan.删除 <shindanCommand:string>\`：删除列表中的一个神断，需指定神断指令。
+- \`shindan.修改 <shindanCommand:string> <shindanNewCommand:string> [shindanMode:string]\`：修改列表中的一个神断，需指定原神断指令、新神断指令和神断模式（默认为原神断模式）。
+- \`shindan.设置 <shindanCommand:string> <shindanMode:string>\`：设置列表中的一个神断的模式，需指定神断指令和神断模式。
 `
 const logger = new Logger(`shindanMaker`)
 
@@ -49,16 +54,40 @@ export interface Config {
 }
 
 // config
-export const Config: Schema<Config> = Schema.object({
-  shindanUrl: Schema.string().default('en.shindanmaker').description(`神断 url，可选前缀有：en, kr, cn, th, 无前缀（en 没被墙）。`),
-  maxRetryCount: Schema.number().min(1).default(3).description(`最大重试次数。`),
-  defaultMaxDisplayCount: Schema.number().min(0).default(20).description(`排行榜默认显示的人数，默认值为 \`20\`。`),
-  defaultShindansBatchCount: Schema.number().min(1).max(10).default(5).description(`发送神断列表默认的批次数，最大值为 \`10\`，默认为 \`5\`。`),
-  imageType: Schema.union(['png', 'jpeg', 'webp']).default('png').description(`图片类型。`),
-  isRandomDivineCommandVisible: Schema.boolean().default(true).description(`随机神断的时候是否显示神断指令名，默认为 \`true\`。`),
-  shouldMiddlewareInterruptAfterDivineDirective: Schema.boolean().default(true).description(`中间件是否在获取神断指令之后中断，默认为 \`true\`。`),
-  isOfficialShindanSyncEnabled: Schema.boolean().default(true).description(`是否与插件内置神断保持同步，关闭后，将不会再为你新增任何神断，默认为 \`true\`。`),
-})
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    isOfficialShindanSyncEnabled: Schema.boolean().default(true).description(`是否与插件内置神断保持同步，关闭后，将不会再为你新增任何神断，默认为 \`true\`。`),
+
+  }).description('神断同步设置'),
+  Schema.object({
+    shindanUrl: Schema.string().default('en.shindanmaker').description(`神断 url，可选前缀有：en, kr, cn, th, 无前缀（en 没被墙）。`),
+
+  }).description('神断网 URL'),
+  Schema.object({
+    maxRetryCount: Schema.number().min(1).default(3).description(`最大重试次数。`),
+
+  }).description('请求设置'),
+  Schema.object({
+    imageType: Schema.union(['png', 'jpeg', 'webp']).default('png').description(`图片类型。`),
+
+  }).description('图片模式设置'),
+  Schema.object({
+    isRandomDivineCommandVisible: Schema.boolean().default(true).description(`随机神断的时候是否显示神断指令名，默认为 \`true\`。`),
+
+  }).description('随机神断设置'),
+  Schema.object({
+    defaultMaxDisplayCount: Schema.number().min(0).default(20).description(`排行榜默认显示的人数，默认值为 \`20\`。`),
+
+  }).description('排行榜设置'),
+  Schema.object({
+    defaultShindansBatchCount: Schema.number().min(1).max(10).default(5).description(`发送神断列表默认的批次数，最大值为 \`10\`，默认为 \`5\`。`),
+
+  }).description('神断列表设置'),
+  Schema.object({
+    shouldMiddlewareInterruptAfterDivineDirective: Schema.boolean().default(true).description(`中间件是否在获取神断指令之后中断，默认为 \`true\`。`),
+
+  }).description('中间件设置'),
+])
 
 type MakeShindanMode = "image" | "text";
 
